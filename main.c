40f89a149755498b821b6185e93f0e86b8b33340
0506victor4 <victor.hernandez21@uabc.edu.mx>
2024-09-25 18:19:57 +0000

    Program 1 completed

--- main.c
+++ main.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
-//Description : Develop a program in which you enter the salary of 10 employees and then obtain the net salary
+// Description: Develop a program in which you enter the salary of 10 employees and then obtain the net salary
 
 int main(void) {
+    float salaries[10];
+    float total = 0;
 
-float salaries[10];
-float total = 0;
-
-  for (int x = 0; x<=10; x++){
-printf("Enter salarie of the employee : "x+1);
-scanf("%f",&salaries[x]);
-  }
-
+    for (int x = 0; x < 10; x++) {
+        printf("Enter the salary of employee number %d: ", x + 1);
+        scanf("%f", &salaries[x]);
+        total += salaries[x]; 
+    }
   
-
+    printf("The total salary of all employees is: %.2f\n", total);
   
-
-  return 0;
+    return 0;
 }
