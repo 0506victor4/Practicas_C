a73e4c06a5e46eb68ef289cb471abbf6f7b99aa6
0506victor4 <victor.hernandez21@uabc.edu.mx>
2024-09-25 18:08:42 +0000

    Program 1

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+//Description : Develop a program in which you enter the salary of 10 employees and then obtain the net salary
+
 int main(void) {
-  printf("Hello World\n");
+
+float salaries[10];
+float total = 0;
+
+  for (int x = 0; x<=10; x++){
+printf("Enter salarie of the employee : "x+1);
+scanf("%f",&salaries[x]);
+  }
+
+  
+
+  
+
   return 0;
 }
