a803a4725965ff4bdeac4aa7e02ea901c356195d
0506victor4 <victor.hernandez21@uabc.edu.mx>
2024-09-25 19:13:13 +0000

    Program 2 completed

--- .ccls-cache/@home@runner@C/main.c
+++ .ccls-cache/@home@runner@C/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+//Description: A marathon runner has covered a distance of 42,195 km in 72 hours and 25 minutes, obtaining the average distance of meters traveled per minute, that is, how many meters he covers per minute.
+
 int main(void) {
-  printf("Hello World\n");
-  return 0;
+
+int kilometers = 42195;
+int hours = 72;
+int minutes = 25;
+
+   float meters = kilometers * 1000;
+    float total_minutes = hours * 60 + minutes;
+
+    float average_speed = meters / total_minutes;
+
+    printf("%f",average_speed);
+    
+    return 0;
 }
--- main.c
+++ main.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
-// Description: Develop a program in which you enter the salary of 10 employees and then obtain the net salary
+//Description: A marathon runner has covered a distance of 42,195 km in 72 hours and 25 minutes, obtaining the average distance of meters traveled per minute, that is, how many meters he covers per minute.
 
 int main(void) {
-    float salaries[10];
-    float total = 0;
 
-    for (int x = 0; x < 10; x++) {
-        printf("Enter the salary of employee number %d: ", x + 1);
-        scanf("%f", &salaries[x]);
-        total += salaries[x]; 
-    }
-  
-    printf("The total salary of all employees is: %.2f\n", total);
-  
+int kilometers = 42195;
+int hours = 72;
+int minutes = 25;
+
+   float meters = kilometers * 1000;
+    float total_minutes = hours * 60 + minutes;
+
+    float average_speed = meters / total_minutes;
+
+    printf("%f",average_speed);
+    
     return 0;
 }
